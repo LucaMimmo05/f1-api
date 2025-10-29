f9233b7327213cfb0641f8cc169feaa51c1366d2
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 10:17:48 +0100

    feat: implementato componente lista Gran Premi con filtro anno e card PrimeNG

--- src/app/gran-premi/gran-premi.ts
+++ src/app/gran-premi/gran-premi.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { F1Service } from '../service/f1-service';
+import { GranPremio } from '../gran-premio/gran-premio';
+import { CardModule } from 'primeng/card';
+import { Meeting } from '../models/metting';
+import { CommonModule } from '@angular/common';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gran-premi',
-  imports: [],
+  imports: [GranPremio, CardModule, CommonModule, SelectModule, FormsModule, ButtonModule],
   templateUrl: './gran-premi.html',
-  styleUrl: './gran-premi.css'
+  styleUrl: './gran-premi.css',
 })
-export class GranPremi {
+export class GranPremi implements OnInit {
+  f1Service = inject(F1Service);
+  meetings: Meeting[] = [];
+  years: number[] = [2024, 2023];
+  selectedYear: number = 2024;
+  router = inject(Router);
 
+  ngOnInit() {
+    this.getMeetings(this.selectedYear);
+  }
+
+  async getMeetings(year: number) {
+    this.meetings = await this.f1Service.getMeetings(year);
+  }
+
+  onYearChange() {
+    this.getMeetings(this.selectedYear);
+  }
+
+  onDetailsClick(meeting : Meeting) {
+    this.router.navigate(['/gran-premio', meeting.meeting_key]);
+  }
 }
