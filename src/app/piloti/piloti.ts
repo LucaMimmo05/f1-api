dee530f1ca14c254a3f82dc8ef903e0fb42dafda
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 12:52:04 +0100

    feat: implementa componente piloti con filtro duplicati e card styling

--- src/app/piloti/piloti.ts
+++ src/app/piloti/piloti.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { F1Service } from '../service/f1-service';
+import { Driver } from '../models/driver';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-piloti',
-  imports: [],
+  imports: [CardModule],
   templateUrl: './piloti.html',
-  styleUrl: './piloti.css'
+  styleUrl: './piloti.css',
 })
 export class Piloti {
+  f1Service = inject(F1Service);
+  drivers: Driver[] = [];
+  filteredDrivers: Driver[] = [];
 
+  ngOnInit() {
+    this.getDrivers();
+  }
+
+  async getDrivers() {
+    this.drivers = await this.f1Service.getDriversWithoutMeeting();
+
+    const uniqueDriversMap = new Map<number, Driver>();
+    this.drivers.forEach((driver) => {
+      if (!uniqueDriversMap.has(driver.driver_number)) {
+        uniqueDriversMap.set(driver.driver_number, driver);
+      }
+    });
+    this.filteredDrivers = Array.from(uniqueDriversMap.values());
+  }
 }
