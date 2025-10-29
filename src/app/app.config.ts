6c2574b9c0a8df0a5b05c45ad5e8c555a382bab1
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 10:16:43 +0100

    feat: configurato routing e HttpClient per l'applicazione

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,23 +4,22 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     providePrimeNG({
-      theme: {
-        preset: 'Aura',
-        options: {
-          prefix: 'p',
-          darkModeSelector: 'white',
-        },
-      },
+      theme: { preset: Aura, options: { prefix: 'p', darkModeSelector: '.dark-theme' } },
+      ripple: true,
     }),
+    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient()
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,7 @@ import { GranPremi } from './gran-premi/gran-premi';
 import { GranPremio } from './gran-premio/gran-premio';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home,
-        title: 'Home'
-    },
-    {
-        path: 'gran-premi',
-        component: GranPremi,
-        title: 'Gran Premi'
-    },
-    {
-        path: 'gran-premio/:id',
-        component: GranPremio,
-        title: 'Gran Premio'
-    }
-
+  { path: '', component: Home, title: 'Home' },
+  { path: 'gran-premi', component: GranPremi, title: 'Gran Premi' },
+  { path: 'gran-premio/:id', component: GranPremio, title: 'Gran Premio' },
 ];
