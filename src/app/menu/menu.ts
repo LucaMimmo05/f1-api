4ac7a6860b17dd70bee6019d0d87e5d948607ba3
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 12:14:20 +0100

    feat: implementato toggle tema scuro/chiaro funzionante nel menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MenubarModule, ToggleSwitchModule, RouterLink],
+  imports: [MenubarModule, ToggleSwitchModule, FormsModule, CommonModule],
   templateUrl: './menu.html',
   styleUrls: ['./menu.css'],
 })
@@ -14,5 +16,25 @@ export class Menu {
   menuItems = [
     { label: 'Home', routerLink: '/' },
     { label: 'Gran Premi', routerLink: '/gran-premi' },
+    { label: 'Piloti', routerLink: '/piloti' },
   ];
+
+  isDarkTheme: boolean = false;
+
+  ngOnInit() {
+    this.isDarkTheme = localStorage.getItem('theme') === 'dark';
+  }
+
+  handleTheme() {
+    const element = document.querySelector('html');
+    if (element) {
+      if (this.isDarkTheme) {
+        localStorage.setItem('theme', 'dark');
+        element.classList.add('dark');
+      } else {
+        localStorage.setItem('theme', 'light');
+        element.classList.remove('dark');
+      }
+    }
+  }
 }
