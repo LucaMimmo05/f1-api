fe600f22ca95aa730107ee110060d2b699a5c179
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 17:44:46 +0100

    feat: configura tema rosso F1 per tutti i componenti PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,21 +5,46 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
+const F1RedPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{red.50}',
+      100: '{red.100}',
+      200: '{red.200}',
+      300: '{red.300}',
+      400: '{red.400}',
+      500: '{red.500}',
+      600: '{red.600}',
+      700: '{red.700}',
+      800: '{red.800}',
+      900: '{red.900}',
+      950: '{red.950}',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     providePrimeNG({
-      theme: { preset: Aura, options: { prefix: 'p', darkModeSelector: '.dark' } },
+      theme: {
+        preset: F1RedPreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark',
+        },
+      },
       ripple: true,
     }),
     provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
   ],
 };
