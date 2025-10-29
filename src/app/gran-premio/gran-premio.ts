7b17c43019242f50d251006f4c0b9211ee24f6cc
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 17:46:01 +0100

    feat: aggiungi formattazione date/ora e navigazione a session-results in gran-premio

--- src/app/gran-premio/gran-premio.ts
+++ src/app/gran-premio/gran-premio.ts
@@ -1,25 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { F1Service } from '../service/f1-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Meeting } from '../models/metting';
 import { Session } from '../models/session';
 import { Driver } from '../models/driver';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { SessionResult } from '../models/session-result';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-gran-premio',
-  imports: [CardModule, CommonModule],
+  imports: [CardModule, CommonModule, TableModule, ButtonModule],
   templateUrl: './gran-premio.html',
   styleUrl: './gran-premio.css',
 })
 export class GranPremio implements OnInit {
   f1Service = inject(F1Service);
   route = inject(ActivatedRoute);
-  meetingKey: string | null = this.route.snapshot.paramMap.get('id');
+  meetingKey: number | null = Number(this.route.snapshot.paramMap.get('id'));
   meeting: Meeting | null = null;
   sessions: Session[] | null = null;
   drivers: Driver[] | null = null;
+  sessionResults: SessionResult[] | null = null;
+  router = inject(Router);
 
   ngOnInit() {
     if (this.meetingKey) {
@@ -29,15 +34,41 @@ export class GranPremio implements OnInit {
     }
   }
 
-  async getMeetingDetails(meetingKey: string) {
+  async getMeetingDetails(meetingKey: number) {
     this.meeting = await this.f1Service.getMeeting(meetingKey);
   }
 
-  async getSessionDetails(meetingKey: string) {
+  async getSessionDetails(meetingKey: number) {
     this.sessions = await this.f1Service.getSessions(meetingKey);
   }
 
-  async getDriverDetails(meetingKey: string) {
-    this.drivers = await this.f1Service.getDrivers(meetingKey);
+  async getDriverDetails(meetingKey: number) {
+    this.drivers = await this.f1Service.getDriversPerMeetingKey(meetingKey);
+  }
+
+  async getSessionResults(sessionKey: number) {
+    this.sessionResults = await this.f1Service.getSessionResults(sessionKey);
+  }
+  
+
+  getDate(dateStart: string): string {
+    const date = new Date(dateStart);
+    return date.toLocaleDateString('it-IT', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+  }
+
+  getTime(dateStart: string): string {
+    const date = new Date(dateStart);
+    return date.toLocaleTimeString('it-IT', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
+
+  goToSessionResults(sessionKey: number) {
+    this.router.navigate(['/session-results', sessionKey]);
   }
 }
