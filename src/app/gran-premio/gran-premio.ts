fae875f543eab7c80a8416806cd0b9f1b15a3bec
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-10-29 10:17:57 +0100

    feat: implementato componente dettaglio Gran Premio con routing dinamico

--- src/app/gran-premio/gran-premio.ts
+++ src/app/gran-premio/gran-premio.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { F1Service } from '../service/f1-service';
+import { ActivatedRoute } from '@angular/router';
+import { Meeting } from '../models/metting';
 
 @Component({
   selector: 'app-gran-premio',
-  imports: [],
+  imports: [CardModule],
   templateUrl: './gran-premio.html',
-  styleUrl: './gran-premio.css'
+  styleUrl: './gran-premio.css',
 })
-export class GranPremio {
+export class GranPremio implements OnInit {
+  f1Service = inject(F1Service);
+  meetingKey: string | null = null;
+  route = inject(ActivatedRoute);
+  meeting: Meeting | null = null;
 
+  ngOnInit() {
+    this.meetingKey = this.route.snapshot.paramMap.get('id');
+    if (this.meetingKey) {
+      this.f1Service.getMeeting(this.meetingKey).then((meeting) => {
+        this.meeting = meeting;
+      });
+    }
+  }
 }
